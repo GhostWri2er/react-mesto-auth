e38917fb5d86f2b8cea1e3a4aef01dd4b1fd0623
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2022-06-21 20:42:03 +0500

    feat: 4. Реализуйте аутентификацию пользователя

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,1 +1,35 @@
 export const baseUrl = 'https://auth.nomoreparties.co';
+
+export const register = (password, email) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ password, email }),
+  }).then((res) => checkResponse(res));
+};
+
+export const login = (password, email) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ password, email }),
+  }).then((res) => checkResponse(res));
+};
+
+export const getToken = (JWT) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${JWT}`,
+    },
+  }).then((res) => checkResponse(res));
+};
+
+function checkResponse(res) {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}
